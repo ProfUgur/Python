1094c23019c4cb3d1a14e535740fcc176ba6822a
ProfUgur <148859613+ProfUgur@users.noreply.github.com>
2023-10-28 12:46:34 +0300

    dorduncu commit

--- asal_carpan.py
+++ asal_carpan.py
@@ -5,18 +5,17 @@ sayi = int(input("\nBir sayı giriniz:"))#Kullanıcıdan sayı alınır
 #Girilen sayının tüm bölenlerinin yazılacağı liste
 tum_bolenler = []
 
-#Sayının tüm bölenlerini hesaplayan fonksiyon
-def fbolen(sayi):    
-    for i in range(1,sayi):
-        if sayi % i == 0:
-            tum_bolenler.append(i)
-    return tum_bolenler
+#Sayının tüm bölenlerini hesaplayan döngü
+for i in range(1,sayi+1):
+    if sayi % i == 0:
+        tum_bolenler.append(i)
+    
 
 #Asal bölenlerin yazılacağı liste
 asal_bolenler = []
 
 #Tüm bölenler içinden sadece asal olanları seçmek için aşağıdaki döngü kullanılır
-for i in fbolen(sayi):
+for i in tum_bolenler:
     toplam = 0#Koşul değişkeni oluşturulur
 
     if i in {0,1}:#i=0 ve i=1 değerleri listeye eklenmez
@@ -27,6 +26,7 @@ for i in fbolen(sayi):
         for j in range(2,i):#2'den bölen sayıya kadar döngü oluşturulur(her bölen için ayrı bir döngü oluşturulur)
             if i % j == 0:#Eğer bölen başka sayılara da tam bölünüyorsa asal değildir(toplamı 1 arttırır)
                 toplam += 1
+                break
         if toplam == 0:#for j döngüsünde toplamı arttırmayan değişken listeye eklenir
             asal_bolenler.append(i)
 
